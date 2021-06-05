721b63f9ffe0abfb90bad7aadc6c9bd230d281fa
Mohammed Yousuf <yusuffaseel@gmail.com>
2021-06-05 23:18:22 +0530

    Initiation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+//pages
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SingleCocktail from "./pages/SingleCocktail";
+import Error from "./pages/Error";
+
+//comps
+import Navbar from "./components/Navbar";
+
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-
+      <h2>app component</h2>
     </div>
   );
 }
